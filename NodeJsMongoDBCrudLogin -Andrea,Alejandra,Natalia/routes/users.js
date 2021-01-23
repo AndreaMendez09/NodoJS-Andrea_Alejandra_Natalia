bd29bbd73f851e1a3f10ab164087d1a8b2e4b942
Andrea <andrea.mendez.sanz01@gmail.com>
2021-01-23 12:34:06 +0100

    Cambiamos de proyecto, y empezamos a mover las vistas de sitio

---
+++ NodeJsMongoDBCrudLogin -Andrea,Alejandra,Natalia/routes/users.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+})); 
+
+router.get('/_login', (req, res, next) => {
+  res.render('_login');
+});
+
+router.get('/_asignaturas', (req, res, next) => {
+  res.render('_asignaturas');
+});
+
+router.get('/editar', (req, res, next) => {
+  res.render('editar');
+});
+
+
+router.post('/signin', passport.authenticate('local-signin', {
+  successRedirect: '/profile',
+  failureRedirect: '/signin',
+  failureFlash: true
+}));
+
+router.get('/profile',isAuthenticated, (req, res, next) => {
+  res.render('profile');
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
+
+
+function isAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/')
+}
+
+module.exports = router;
