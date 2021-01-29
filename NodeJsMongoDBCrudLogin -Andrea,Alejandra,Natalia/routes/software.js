97021ef9cb744378a5599315b90ab7e70ce41b73
Andrea <andrea.mendez.sanz01@gmail.com>
2021-01-29 14:25:15 +0100

    La numeracion va bien, añadido campos de añadir en software

--- NodeJsMongoDBCrudLogin -Andrea,Alejandra,Natalia/routes/software.js
+++ NodeJsMongoDBCrudLogin -Andrea,Alejandra,Natalia/routes/software.js
@@ -12,6 +12,18 @@ router.get('/software/:id', isAuthenticated, async (req, res, next) => {
 });
 
 
+router.post('/software/add',async (req, res, next) => {
+  console.log(req.body);
+  /* Creamos un objeto de tipo asignaturas con el form*/ 
+  const software = new software(req.body);
+  console.log(software);
+  /*Guardamos el usuario */
+  await software.save();
+  /*Devolvemos los datos a la vista*/
+  res.redirect('/software');
+});
+
+
 function isAuthenticated(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
